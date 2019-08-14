672379cb4128e3b3d4dbf7499a5778724aca006a
Jesse Gay <jesse@teamtreehouse.com>
2019-08-13 17:28:56 -0700

    Added project starter files
    From
    https://teamtreehouse.com/library/build-a-quiz-challenge-part-1

---
+++ js/quiz.js
@@ -0,0 +1,30 @@
+
+function print(message) {
+  document.write(message);
+}
+
+var correctResponses = 0
+var questions = [];
+var question0 = ['On what street is Treehouse?', 'Vancouver'];
+var question1 = ['What punctuation mark ends most JavaScript statements? Type the actual symbol.', ';'];
+questions.push(question0, question1);
+
+
+// ask a question
+var response0 = prompt(question0[0])
+
+
+// evaluate answer
+
+
+
+// keep track of correct responses
+
+
+// repeat for all questions
+
+
+// when program is done, display number of correct responses
+
+
+
